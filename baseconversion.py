c6b8381fcd4443be985d3dc1f92f1582a907a1ff
yousefakbar <yousef_akbar@me.com>
2019-04-19 20:07:27 -0700

    added input validation

--- baseconversion.py
+++ baseconversion.py
@@ -1,18 +1,48 @@
 # python recursive base conversion
 
+symbolDict = {
+    "A": "10",
+    "B": "11",
+    "C": "12",
+    "D": "13",
+    "E": "14",
+    "F": "15"
+}
+
+def validateNum(inputNum):
+    #accept if each digit is either:
+    #   ~ a number
+    #   ~ a character ie isalpha()
+    #   ~ a decimal point
+    #otherwise reject
+
+    for digit in inputNum:
+        if (digit.isdigit()) or (digit.isalpha()) or (digit=="."):
+            continue
+        else:
+            print("Invalid Input. Try Again (Must be letter, digit, or period")
+            exit()        
+
 def convertToB10(inputNum, inputBase):
     numDigits = len(inputNum)
     B10num = 0
 
     for i in range(0, numDigits):
+        if inputNum[i].isalpha():
+            coeff = symbolDict[inputNum[i].upper()]
+        else:
+            coeff = inputNum[i]
+        
         exp = (numDigits-1) - i
-        nextDigit = int(inputNum[i]) * (inputBase**exp)
+        nextDigit = int(coeff) * (inputBase**exp)
         B10num += nextDigit
 
     return str(B10num)
         
 
 def convert(inputNum, inputBase, outputBase):
+    validateNum(inputNum)
+
     B10num = convertToB10(inputNum, inputBase)
 
     if outputBase == 10:
